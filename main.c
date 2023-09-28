48d7b2f696afb2bc3ae27153b375972949fbb800
minseo0520 <141472390+minseo0520@users.noreply.github.com>
2023-09-28 19:43:20 +0900

    실습3

--- main.c
+++ main.c
@@ -3,17 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int a, b;
+int main(int argc, char *argv[]){
+	int a;
 	
-	printf("input two integers: ");
-	scanf("%i %i", &a, &b);
+	printf("input the second: ");
+	scanf("%i", &a);
 	
-	printf("%i + %i = %i\n", a, b, a+b);
-	printf("%i - %i = %i\n", a, b, a-b);
-	printf("%i * %i = %i\n", a, b, a*b);
-	printf("%i / %i = %i\n", a, b, a/b);
-	printf("%i %% %i = %i\n", a, b, a%b);
+	printf("%i seconds - %i:%i \n", a, a/60, a%60);
 	
 	return 0;
 }
